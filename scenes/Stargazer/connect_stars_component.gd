184584d9514ec75f85ca8828dcaf2d6ac063c40a
Dylore Studios <dylorestudios@gmail.com>
2024-08-07 22:53:24 +0800

    Code Fixes and Comments

--- scenes/Stargazer/connect_stars_component.gd
+++ scenes/Stargazer/connect_stars_component.gd
@@ -8,61 +8,73 @@ var line: Line2D
 
 var stars_array
 
-var can_point = false
-var inside_star = false
-var first_point = true
+var is_inside_star = false
+var is_first_point = true
 var is_building = true
 
 var target_point_position: Vector2
 var entered_star: Area2D
 
+# on ready, we connect all the stars by their mouse entered and mouse exited signals
 func _ready():
 	connect_stars_to_signal()
 
-func _input(event):
-	if Input.is_action_just_pressed("left_click") and can_point:
-		if first_point:
-			line = line_instance.instantiate()
-			star_parallax.add_child(line)
-			star_parallax.move_child(line, 0)
-			is_building = true
-			line.add_point(target_point_position)
-			line.points[line.get_point_count() - 1] = target_point_position
-			line.add_point(target_point_position)
-			first_point = false
-		else:
-			line.points[line.get_point_count() - 1] = target_point_position
-			line.add_point(target_point_position)
-	elif Input.is_action_just_pressed("left_click") and \
-									is_building and \
-									not can_point and \
-									not inside_star:
-		is_building = false
-		first_point = true
-		if line:
-			if line.points:
-				line.remove_point(line.get_point_count() - 1)
-
+# On process, make the last point follow the mouse
 func _process(delta):
 	point_follow_mouse()
 
-func point_follow_mouse():
-	var mouse_pos = get_global_mouse_position()
-	if line and is_building:
-		if line.points:
-			line.points[line.get_point_count() - 1] = mouse_pos
-
+# We get all children in stars component and connect them to the mouse entered and exited signals
 func connect_stars_to_signal():
 	stars_array = stars.get_children()
 	for star in stars_array:
 		star.mouse_entered.connect(_on_mouse_entered_star.bind(star))
 		star.mouse_exited.connect(_on_mouse_exited_star.bind(star))
 
+# we call this everytime we create a first point
+# we add our line as a child to our star parallax layer
+func add_line():
+	line = line_instance.instantiate()
+	star_parallax.add_child(line)
+	star_parallax.move_child(line, 0)
+	line.add_point(target_point_position)
+
+# we call this when we want to add a point to a certain position
+func add_point_at_position(position):
+	line.points[line.get_point_count() - 1] = position
+	line.add_point(position)
+
+# all events related to inputs
+func _input(event):
+	if Input.is_action_just_pressed("left_click"):
+		# If you are inside a star and you are doing your first point,
+		if is_inside_star and is_first_point:
+			is_building = true
+			is_first_point = false
+			add_line()
+			add_point_at_position(target_point_position)
+		# If you are inside a star and it is not your first point,
+		elif is_inside_star and not is_first_point:
+			add_point_at_position(target_point_position)
+		# If you are not inside a star and you are still building,
+		elif not is_inside_star and is_building:
+			is_building = false
+			is_first_point = true
+			if line:
+				if line.points:
+					line.remove_point(line.get_point_count() - 1)
+
+# Move the point to follow the mouse
+func point_follow_mouse():
+	var mouse_pos = get_global_mouse_position()
+	if line and is_building:
+		if line.points:
+			line.points[line.get_point_count() - 1] = mouse_pos
+
+# Update the target_point_position and check the is_inside_star
 func _on_mouse_entered_star(star):
-	inside_star = true
-	can_point = true
+	is_inside_star = true
 	target_point_position = star.position
 
+# turn is_inside_star to false
 func _on_mouse_exited_star(star):
-	inside_star = false
-	can_point = false
+	is_inside_star = false
--- scenes/Stargazer/stargazer_camera.gd
+++ scenes/Stargazer/stargazer_camera.gd
@@ -1,6 +1,6 @@
 extends Camera2D
 
-@export var camera_speed: float = 1.0
+@export var camera_speed: float = 0.3
 @export var space_size: Vector2 = Vector2(2000, 2000)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -8,10 +8,14 @@ func _process(delta):
 	move_to_mouse_pos(delta)
 	stop_on_space_border()
 
+# we lerp the global position of the camera to the position of the mouse by the camera_speed
 func move_to_mouse_pos(delta):
 	var mouse_pos = get_global_mouse_position()
 	global_position = lerp(global_position, mouse_pos, delta * camera_speed)
 
+# We have to clamp the the position of the camera to the minimum and maximum of a border
+# This is a variable which will be taken from the telescope data. Each telescope has a
+# different size of border.
 func stop_on_space_border():
 	var minimum = -(space_size/2) + (get_viewport_rect().size/2)
 	var maximum = (space_size/2) - (get_viewport_rect().size/2)
