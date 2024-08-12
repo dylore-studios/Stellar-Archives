57387b28a28c6f64ca3d51c4291f1826f43a39e9
dylore-studios <dylorestudios@gmail.com>
2024-08-12 08:27:53 +0800

    FINALLY, Stargazers are local to Telescopes

--- scenes/Stargazer/stargazer_data.gd
+++ scenes/Stargazer/stargazer_data.gd
@@ -1,3 +1,8 @@
 extends Node2D
 
 @export var space_size := Vector2(2000, 2000)
+@export var world = "res://world.tscn"
+
+func _input(event):
+	if Input.is_action_just_pressed("sprint"):
+		pass
--- scenes/Telescope/StargazerComponent.gd
+++ scenes/Telescope/StargazerComponent.gd
@@ -2,9 +2,13 @@ extends Node
 
 @export var telescope_animation : AnimationPlayer
 @export var lookbox : Area3D
-@export var stargazer : Sprite3D
+@export var stargazer : PackedScene
 
-var stargazer_is_open = true
+var stargazer_instance : Node2D
+
+var stargazer_is_open := true
+var first_time_open := true
+var player_is_close := false
 
 func _ready():
 	lookbox.body_entered.connect(_on_look_box_body_entered)
@@ -12,18 +16,26 @@ func _ready():
 	telescope_animation.play("close_telescope")
 
 func _input(event):
-	if Input.is_action_just_pressed("sprint"):
-		if stargazer_is_open:
+	if Input.is_action_just_pressed("right_click"):
+		if stargazer_is_open and player_is_close:
 			stargazer_is_open = false
-			stargazer.hide()
-		else:
+			if first_time_open:
+				generate_stargazer()
+				first_time_open = false
+			add_child(stargazer_instance)
+		elif not stargazer_is_open and player_is_close:
 			stargazer_is_open = true
-			stargazer.show()
+			remove_child(stargazer_instance)
+
+func generate_stargazer():
+	stargazer_instance = stargazer.instantiate()
 
 func _on_look_box_body_entered(body):
 	if body.is_in_group("Player"):
+		player_is_close = true
 		telescope_animation.play("open_telescope")
 
 func _on_look_box_body_exited(body):
 	if body.is_in_group("Player"):
+		player_is_close = false
 		telescope_animation.play("close_telescope")
